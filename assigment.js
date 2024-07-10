a8cb85b3d92d618641496915f0465b6c716d284d
Maryam6713 <maryammalik6713@gmail.com>
2024-07-10 17:32:06 +0500

    first commit

---
+++ assigment.js
@@ -0,0 +1,32 @@
+//--------question1-----------//
+// var fname = prompt("enter your first name");
+// var lname = prompt("enter your last name");
+// var fullName = fname + " " +lname;
+// alert("Hello " + fullName)
+
+//----------question2--------------//
+
+// var userInput = prompt("enter your favorite mobile model");
+// var inputLenght = userInput.length
+// console.log(inputLenght);
+
+//----------question3------------//
+
+// var str = "Pakistan"
+// console.log(str);
+// var place = str.charAt(7)
+// console.log(place);
+
+//----------question4----------//
+
+// var str2 = "Hello world";
+// console.log(str2);
+// var place2 = str2.lastIndexOf("l")
+// console.log(place2);
+
+//--------question5-----------//
+
+var identity = "Pakistani";
+console.log(identity);
+var find = identity.indexOf("i")
+console.log(find);
