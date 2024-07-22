fb9683410a144b09e7569d7deb921c17c7fdd60c
Maryam6713 <maryammalik6713@gmail.com>
2024-07-22 16:36:15 +0500

    initial commit

--- assigment.js
+++ assigment.js
@@ -1,94 +1,94 @@
 //--------question1-----------//
-// var fname = prompt("enter your first name");
-// var lname = prompt("enter your last name");
-// var fullName = fname + " " +lname;
-// alert("Hello " + fullName)
+var fname = prompt("enter your first name");
+var lname = prompt("enter your last name");
+var fullName = fname + " " +lname;
+alert("Hello " + fullName)
 
 //----------question2--------------//
 
-// var userInput = prompt("enter your favorite mobile model");
-// var inputLenght = userInput.length
-// console.log(inputLenght);
+var userInput = prompt("enter your favorite mobile model");
+var inputLenght = userInput.length
+console.log(inputLenght);
 
 //----------question3------------//
 
-// var str = "Pakistan"
-// console.log(str);
-// var place = str.charAt(7)
-// console.log(place);
+var str = "Pakistan"
+console.log(str);
+var place = str.charAt(7)
+console.log(place);
 
 //----------question4----------//
 
-// var str2 = "Hello world";
-// console.log(str2);
-// var place2 = str2.lastIndexOf("l")
-// console.log(place2);
+var str2 = "Hello world";
+console.log(str2);
+var place2 = str2.lastIndexOf("l")
+console.log(place2);
 
 //--------question5-----------//
 
-// var identity = "Pakistani";
-// console.log(identity);
-// var find = identity.indexOf("i")
-// console.log(find);
+var identity = "Pakistani";
+console.log(identity);
+var find = identity.indexOf("i")
+console.log(find);
 
 //---------question6-------------//
 
-// var fname = prompt("enter your first name");
-//  var lname = prompt("enter your last name");
-// var concatName = fname.concat(lname);
-// alert("Hello " + concatName);
+var fname = prompt("enter your first name");
+ var lname = prompt("enter your last name");
+var concatName = fname.concat(lname);
+alert("Hello " + concatName);
 
 //-----------question7--------------//
 
-// var countryName = "Hyderabad"
-// var replaceName = countryName.replace("Hyder" , "Islam")
-// console.log(countryName);
-// console.log(replaceName)
+var countryName = "Hyderabad"
+var replaceName = countryName.replace("Hyder" , "Islam")
+console.log(countryName);
+console.log(replaceName)
 
 //-----------question8-----------//
 
-// var message = "Ali and Sami are best friends. They play cricket and football together";
-// var replaceWord = message.replaceAll("and", "&")
-// console.log(message);
-// console.log(replaceWord);
+var message = "Ali and Sami are best friends. They play cricket and football together";
+var replaceWord = message.replaceAll("and", "&")
+console.log(message);
+console.log(replaceWord);
 
 //-----------question9-----------//
 
-// var str = "472"
-// console.log(str);
-// console.log(typeof(str));
-// var changeNumber = parseInt(str)
-// console.log(changeNumber);
-// console.log(typeof(changeNumber));
+var str = "472"
+console.log(str);
+console.log(typeof(str));
+var changeNumber = parseInt(str)
+console.log(changeNumber);
+console.log(typeof(changeNumber));
 
 //----------question10---------//
 
-// var userInput = prompt("enter any word or sentence")
-// var capital = userInput.toUpperCase()
-// console.log(capital);
+var userInput = prompt("enter any word or sentence")
+var capital = userInput.toUpperCase()
+console.log(capital);
 
 //------------question11-------------//
 
-// var word = prompt("enter any word")
-// var titleCase = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-// console.log(titleCase);
+var word = prompt("enter any word")
+var titleCase = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+console.log(titleCase);
 
 //------------question12-----------//
 
-// var num = 35.36 ;
-// var num2 = num.toString().replace("." , "")
-// console.log(num2);
-// var str = parseInt(num2)
-// console.log(str);
+var num = 35.36 ;
+var num2 = num.toString().replace("." , "")
+console.log(num2);
+var str = parseInt(num2)
+console.log(str);
 
 //------------question13---------//
 
-// var userName = prompt("enter user name")
-// if(userName.includes("$") || userName.includes("@") || userName.includes("!") || userName.includes("&")){
-//     alert("please enter a valid username without using symbol \n $ , @ , ! or &")
-// }else{
-//     alert("username accepted" + "" + userName)
-// }
+var userName = prompt("enter user name")
+if(userName.includes("$") || userName.includes("@") || userName.includes("!") || userName.includes("&")){
+    alert("please enter a valid username without using symbol \n $ , @ , ! or &")
+}else{
+    alert("username accepted" + "" + userName)
+}
 
 //----------question14----------//
 
@@ -107,3 +107,46 @@ if(found){
 }else{
     alert( " is not available in our bakery")
 }
+
+//--------------question15------------//
+var password = prompt("Please enter a password:");
+
+for (;;) {
+    if (password.length < 6 || !isNaN(password[0]) || (!/[a-zA-Z]/.test(password) || !/\d/.test(password))) {
+        alert("Password requirements not met. Please enter a valid password.");
+        password = prompt("Please enter a valid password:");
+    } else {
+        alert("Valid password accepted.");
+        break;
+    }
+}
+
+//-----------question16---------//
+
+var university = "University of Karachi"
+ var uniArr = university.split("")
+for(var i = 0 ; i < uniArr.length ; i++){
+    document.write(uniArr[i] + "</br>")
+}
+
+//------------question17-----------//
+
+var userInp = prompt("enter a word")
+var lastChar = userInp.slice(-1)
+console.log("Word:" + userInp);
+console.log("Last Character:" + lastChar);
+
+//------------question18--------//
+
+var sentence = "The quick brown fox jumps over the lazy dog";
+var wordToFind = "the";
+var count = 0;
+
+var words = sentence.split(" ");
+for (var i = 0; i < words.length; i++) {
+    if (words[i].toLowerCase() === wordToFind) {
+        count++;
+    }
+}
+
+document.write("Number of occurrences of the word 'the': " + count);
